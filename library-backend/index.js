724b243f7ae90693399f214ad0feb2f15474ec28
Andrew A Lee <andrew@andrewleedev.com>
2026-08-24 22:34:23 -0700

    Completes exercise 1

--- library-backend/index.js
+++ library-backend/index.js
@@ -28,8 +28,9 @@ let authors = [
 ]
 
 /*
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
+ * It might make more sense to associate a book with its author by storing the author's
+ * id in the context of the book instead of the author's name.
+ * However, for simplicity, we will store the author's name in connection with the book.
  */
 
 let books = [
@@ -88,15 +89,17 @@ let books = [
   you can remove the placeholder query once your first one has been implemented 
 */
 
-const typeDefs = `
+const typeDefs = /* GraphQL */ `
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
   },
 }
 
