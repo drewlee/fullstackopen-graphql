307b96b7869691ab1e5acceb91194a520d0b4733
Andrew A Lee <andrew@andrewleedev.com>
2026-08-24 22:41:51 -0700

    Completes exercise 2

--- library-backend/index.js
+++ library-backend/index.js
@@ -90,9 +90,18 @@ let books = [
 */
 
 const typeDefs = /* GraphQL */ `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: String!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -100,6 +109,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 }
 
