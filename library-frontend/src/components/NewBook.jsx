88195e1a18db999e74fb15f8100a73a4e2f54d6c
Andrew A Lee <andrew@andrewleedev.com>
2026-08-26 19:03:52 -0700

    Completes exercise 10

--- library-frontend/src/components/NewBook.jsx
+++ library-frontend/src/components/NewBook.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client/react'
+import { ALL_BOOKS, ALL_AUTHORS, CREATE_BOOK } from '../queries'
+import './NewBook.css'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,6 +9,9 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  const [createBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ALL_BOOKS, ALL_AUTHORS],
+  })
 
   if (!props.show) {
     return null
@@ -14,7 +20,12 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    if (!genres.length) {
+      alert('Please include at least one genre')
+      return
+    }
+
+    createBook({ variables: { title, author, published: Number(published), genres } })
 
     setTitle('')
     setPublished('')
@@ -30,39 +41,52 @@ const NewBook = (props) => {
 
   return (
     <div>
-      <form onSubmit={submit}>
-        <div>
-          title
+      <form onSubmit={submit} className="book-form">
+        <div className="book-form_field">
+          <label htmlFor="title">title</label>
           <input
+            id="title"
             value={title}
             onChange={({ target }) => setTitle(target.value)}
+            required
           />
         </div>
-        <div>
-          author
+
+        <div className="book-form_field">
+          <label htmlFor="author">author</label>
           <input
+            id="author"
             value={author}
             onChange={({ target }) => setAuthor(target.value)}
+            required
           />
         </div>
-        <div>
-          published
+
+        <div className="book-form_field">
+          <label htmlFor="published">published</label>
           <input
+            id="published"
             type="number"
             value={published}
             onChange={({ target }) => setPublished(target.value)}
+            required
           />
         </div>
-        <div>
+
+        <div className="book-form_field">
           <input
+            aria-label="genre"
             value={genre}
             onChange={({ target }) => setGenre(target.value)}
           />
+
           <button onClick={addGenre} type="button">
             add genre
           </button>
         </div>
+
         <div>genres: {genres.join(' ')}</div>
+
         <button type="submit">create book</button>
       </form>
     </div>
