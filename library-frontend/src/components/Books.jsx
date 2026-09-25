8934eddf71ab70c3d0d3e392bd6a5cc9b9ed2f0c
Andrew A Lee <andrew@andrewleedev.com>
2026-09-25 11:31:26 -0700

    Completes exercise 20

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,16 +1,61 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client/react'
 import { ALL_BOOKS } from '../queries'
 
 const Books = ({ show }) => {
+  const [selectedGenres, setSelectedGenres] = useState([])
   const result = useQuery(ALL_BOOKS)
 
   if (!show || !result.data) {
     return null
   }
 
+  const { allBooks } = result.data
+  const allGenres = allBooks.reduce((genres, book) => {
+    for (const genre of book.genres) {
+      genres.add(genre)
+    }
+    return genres
+  }, new Set())
+  const sortedGenres = [...allGenres].toSorted()
+  const filteredBooks = selectedGenres.length
+    ? allBooks.filter((book) => {
+        for (const genre of book.genres) {
+          if (selectedGenres.includes(genre)) {
+            return true
+          }
+        }
+        return false
+      })
+    : allBooks
+
+  const handleGenreClick = (genre) => {
+    if (selectedGenres.includes(genre)) {
+      setSelectedGenres(selectedGenres.filter((currGenre) => currGenre !== genre))
+    } else {
+      setSelectedGenres([...selectedGenres, genre])
+    }
+  }
+
   return (
     <div>
       <h2>books</h2>
+
+      {sortedGenres.length > 0 && (
+        <div>
+          {sortedGenres.map((genre) => (
+            <button type="button" key={genre} onClick={() => handleGenreClick(genre)}>
+              {genre}
+            </button>
+          ))}
+        </div>
+      )}
+
+      {selectedGenres.length > 0 && (
+        <p>
+          By genre: <strong>{selectedGenres.join(', ')}</strong>
+        </p>
+      )}
 
       <table>
         <tbody>
@@ -19,7 +64,7 @@ const Books = ({ show }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {result.data.allBooks.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
