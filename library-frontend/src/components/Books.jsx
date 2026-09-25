69b165011582bf49d0265a065789bf5b4fd1c658
Andrew A Lee <andrew@andrewleedev.com>
2026-09-24 22:30:03 -0700

    Completes exercise 18

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -22,7 +22,7 @@ const Books = ({ show }) => {
           {result.data.allBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
           ))}
